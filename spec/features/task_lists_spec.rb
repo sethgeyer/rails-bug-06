e7cb032b86f38a6206b320109f276a4b259d27f0
Seth Geyer <seth.geyer@gmail.com>
2014-09-03 10:36:54 -0600

    corrected error in param parts that caused the routing for the 'edit' button to reference only a singleitem

--- spec/features/task_lists_spec.rb
+++ spec/features/task_lists_spec.rb
@@ -2,15 +2,37 @@ require 'rails_helper'
 require 'capybara/rails'
 
 feature 'Task lists' do
-
-  scenario 'User can view task lists' do
+  before(:each) do
     user = create_user email: "user@example.com"
     TaskList.create!(name: "Work List")
     TaskList.create!(name: "Household Chores")
 
     login(user)
+  end
+  scenario 'User can view task lists' do
     expect(page).to have_content("Work List")
     expect(page).to have_content("Household Chores")
   end
+
+  scenario 'User can link to edit task list items' do
+
+    click_on "+ Add Task", match: :first
+    fill_in "Description", with: "Something important"
+    click_on "Create Task"
+
+    click_on "+ Add Task", match: :first
+    fill_in "Description", with: "aaaHaircut"
+    click_on "Create Task"
+
+    expect(page).to have_content("Something important")
+    expect(page).to have_content("aaaHaircut")
+
+    first(".task").click_link("edit")
+    # expect(page).to have_button("Update Task")
+    # expect(page).to have_content("aaaGarage Chores")
+
+  end
+
+
 
 end
